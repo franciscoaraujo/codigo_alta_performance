d9a1fe3fcb828c512ceccb26c26ad21a6c0dd298
franciscoaraujo <chicoaraujo1063@gmail.com>
2022-09-17 18:01:05 -0300

    commitando projeto para o estudos do nano curso de codigo de alta performance

---
+++ codigo_alta_performance/src/main/java/br/com/code/codigo_alta_performance/CodigoAltaPerformanceApplication.java
@@ -0,0 +1,13 @@
+package br.com.code.codigo_alta_performance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodigoAltaPerformanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodigoAltaPerformanceApplication.class, args);
+    }
+
+}
---
+++ codigo_alta_performance/src/test/java/br/com/code/codigo_alta_performance/CodigoAltaPerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.code.codigo_alta_performance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodigoAltaPerformanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
